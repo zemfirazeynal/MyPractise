bfda14315b6b065c16b01ded2eed1592b8f1c3a1
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2024-03-22 16:33:01 +0400

    Arrays

---
+++ src/self/arrays/Main_2.java
@@ -0,0 +1,12 @@
+package self.arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main_2 {
+    public static void main(String[] args) {
+        int []arr ={1,2,3};
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
