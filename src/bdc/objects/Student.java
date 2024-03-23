defc9d56ec95c093d1de33a58b0f629f6af6b3ec
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2024-03-23 13:27:25 +0400

    Objects

---
+++ src/bdc/objects/Student.java
@@ -0,0 +1,19 @@
+package bdc.objects;
+
+public class Student {
+    public String name;
+    public String surname;
+    public int age;
+    public String className;
+    public int classNo;
+
+
+    public static void toStringV1( Student s){
+        System.out.println("name:"+s.name);
+        System.out.println("surname:"+s.surname);
+        System.out.println("age:"+s.age);
+        System.out.println("className:"+s.className);
+        System.out.println("classNo:"+s.classNo);
+
+    }
+}
